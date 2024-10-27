08a15636af36df874a6b8c9b158a1b1905c89254
Nguyen Van Manh <manhk18kma@gmail.com>
2024-10-28 00:35:43 +0700

    init be

---
+++ BeBookingApp/src/main/java/KMA/BeBookingApp/BeBookingAppApplication.java
@@ -0,0 +1,13 @@
+package KMA.BeBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBookingAppApplication.class, args);
+	}
+
+}
---
+++ BeBookingApp/src/test/java/KMA/BeBookingApp/BeBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package KMA.BeBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
